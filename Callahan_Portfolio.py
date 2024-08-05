28a4c68cb8277bf151973a783f2b6b9d990047b0
jcallahan997 <jac421@pitt.edu>
2024-08-05 12:09:28 -0400

    reduce size of processed file

--- Callahan_Portfolio.py
+++ Callahan_Portfolio.py
@@ -4,7 +4,7 @@ st.title("Welcome to my portfolio!")
 
 st.write("Hi, I'm James. I work as a data scientist and am passionate about the field. In my free time, I like to boulder, travel, and dance!")
 #add pic of myself here
-st.image("IMG_5550.jpeg", caption="A picture of me on a recent trip I took with a friend to Sri Lanka.", width=500)
+st.image("IMG_5550.jpeg", caption="A picture of me on a recent trip I took with a friend to Sri Lanka.")
 # present resume and a download button for it here
 
 pdf_viewer("Callahan_resume_07_2024_consulting.pdf")
