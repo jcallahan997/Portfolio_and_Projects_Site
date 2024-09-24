a0d185880abe97d77bd8663788ce2906c4d79978
jcallahan997 <jac421@pitt.edu>
2024-09-24 10:06:40 -0400

    update to latest resume

--- Callahan_Portfolio.py
+++ Callahan_Portfolio.py
@@ -7,9 +7,9 @@ st.write("Hi, I'm James. I work as a data scientist and am passionate about the 
 st.image("IMG_5550.jpeg", caption="A picture of me on a recent trip I took with a friend to Sri Lanka.")
 # present resume and a download button for it here
 
-pdf_viewer("Callahan_resume_07_2024_consulting.pdf")
+pdf_viewer("Callahan_James_Resume(v1.0).pdf")
 
-with open('Callahan_James_Resume.docx', 'rb') as f:
+with open('Callahan_James_Resume(v1.0).docx', 'rb') as f:
    st.download_button('Download resume as .docx', f, file_name='Callahan_James_Resume.docx')
 
 st.write("Check out my GitHub: https://github.com/jcallahan997")
