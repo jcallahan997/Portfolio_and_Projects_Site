672b40dbc69c18bfa5c26b475fd280c288311004
jcallahan997 <jac421@pitt.edu>
2024-09-24 17:19:31 -0400

    test code snippet

--- pages/2_Project:_Toastmasters_Table_Topics_LLM.py
+++ pages/2_Project:_Toastmasters_Table_Topics_LLM.py
@@ -18,153 +18,41 @@ st.write("This app is an Azure OpenAI instance of gpt-35-turbo, that is then set
          Streamlit portfolio. The portfolio is containerized with Docker and runs on an Azure cloud compute\
           instance.")
 
-LLM_API_KEY = os.getenv('API_KEY')
-LLM_ENDPOINT = os.getenv('endpoint')
-DEPLOYMENT_NAME = os.getenv('deployment_name')
+key = os.getenv('API_KEY')
+endpoint = os.getenv('endpoint')
+model_name = os.getenv('deployment_name')
 
 
 openai.api_type = "azure"
-openai.api_base = LLM_ENDPOINT
-openai.api_key = LLM_API_KEY
+openai.api_base = endpoint
+openai.api_key = key
 openai.api_version = "2024-05-01-preview"
 
+client = AzureOpenAI(azure_endpoint=endpoint,api_version="2024-05-01-preview",api_key=key)
 
-llm = AzureOpenAI(
-    deployment_name='gpt-35-turbo_for_table_topics',
-    model_name="gpt-35-turbo", 
-)
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-st.title("ChatGPT-like clone")
-import streamlit as st
-from streamlit_chat import message
-import os
-import openai
-import datetime
-import json
-
-
-# region PROMPT SETUP
-
-default_prompt = """
-You are an AI assistant that helps users write concise\
- reports on sources provided according to a user query.\
- You will give users 10 ideas for Toastmasters Table Topics.\
- Greet the user by asking what they'd like their topic/theme to be.
-"""
-
-system_prompt = st.sidebar.text_area("System Prompt", default_prompt, height=200)
-seed_message = {"role": "system", "content": system_prompt}
-# endregion
-
-# region SESSION MANAGEMENT
-# Initialise session state variables
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-if "past" not in st.session_state:
-    st.session_state["past"] = []
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [seed_message]
-if "model_name" not in st.session_state:
-    st.session_state["model_name"] = []
-if "cost" not in st.session_state:
-    st.session_state["cost"] = []
-if "total_tokens" not in st.session_state:
-    st.session_state["total_tokens"] = []
-if "total_cost" not in st.session_state:
-    st.session_state["total_cost"] = 0.0
-# endregion
+    st.session_state.messages = []
 
-# region SIDEBAR SETUP
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-counter_placeholder = st.sidebar.empty()
-counter_placeholder.write(
-    f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}"
-)
-clear_button = st.sidebar.button("Clear Conversation", key="clear")
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-if clear_button:
-    st.session_state["generated"] = []
-    st.session_state["past"] = []
-    st.session_state["messages"] = [seed_message]
-    st.session_state["number_tokens"] = []
-    st.session_state["model_name"] = []
-    st.session_state["cost"] = []
-    st.session_state["total_cost"] = 0.0
-    st.session_state["total_tokens"] = []
-    counter_placeholder.write(
-        f"Total cost of this conversation: £{st.session_state['total_cost']:.5f}"
-    )
-
-
-download_conversation_button = st.sidebar.download_button(
-    "Download Conversation",
-    data=json.dumps(st.session_state["messages"]),
-    file_name=f"conversation.json",
-    mime="text/json",
-)
-
-# endregion
-
-
-def generate_response(prompt):
-    st.session_state["messages"].append({"role": "user", "content": prompt})
-    try:
-        completion = openai.ChatCompletion.create(
-            engine=llm,
-            messages=st.session_state["messages"],
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
         )
-        response = completion.choices[0].message.content
-    except openai.error.APIError as e:
-        st.write(response)
-        response = f"The API could not handle this content: {str(e)}"
-    st.session_state["messages"].append({"role": "assistant", "content": response})
-
-    # print(st.session_state['messages'])
-    total_tokens = completion.usage.total_tokens
-    prompt_tokens = completion.usage.prompt_tokens
-    completion_tokens = completion.usage.completion_tokens
-    return response, total_tokens, prompt_tokens, completion_tokens
-
-
-st.title("Streamlit ChatGPT Demo")
-
-# container for chat history
-response_container = st.container()
-# container for text box
-container = st.container()
-
-with container:
-    with st.form(key="my_form", clear_on_submit=True):
-        user_input = st.text_area("You:", key="input", height=100)
-        submit_button = st.form_submit_button(label="Send")
-
-    if submit_button and user_input:
-        output, total_tokens, prompt_tokens, completion_tokens = generate_response(
-            user_input
-        )
-        st.session_state["past"].append(user_input)
-        st.session_state["generated"].append(output)
-        st.session_state["total_tokens"].append(total_tokens)
-
-        # from https://azure.microsoft.com/en-us/pricing/details/cognitive-services/openai-service/#pricing
-        cost = total_tokens * 0.001625 / 1000
-
-        st.session_state["cost"].append(cost)
-        st.session_state["total_cost"] += cost
-
-
-if st.session_state["generated"]:
-    with response_container:
-        for i in range(len(st.session_state["generated"])):
-            message(
-                st.session_state["past"][i],
-                is_user=True,
-                key=str(i) + "_user",
-                avatar_style="shapes",
-            )
-            message(
-                st.session_state["generated"][i], key=str(i), avatar_style="identicon"
-            )
-        counter_placeholder.write(
-            f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}"
-        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
