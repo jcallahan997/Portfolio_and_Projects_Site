526f280dc26676f214dbec3b54ed34d1a87d4622
jcallahan997 <jac421@pitt.edu>
2024-09-23 21:27:04 -0400

    fix client definition

--- pages/2_Project:_Toastmasters_Table_Topics_LLM.py
+++ pages/2_Project:_Toastmasters_Table_Topics_LLM.py
@@ -16,39 +16,38 @@ endpoint = os.getenv("ENDPOINT_URL", LLM_ENDPOINT)
 deployment = os.getenv("DEPLOYMENT_NAME", DEPLOYMENT_NAME)
 subscription_key = os.getenv("AZURE_OPENAI_API_KEY", LLM_API_KEY)
 
+st.title("ChatGPT-like clone")
 
-def create_chat_completion(endpoint, subscription_key, deployment, messages):
-    # Initialize Azure OpenAI client with key-based authentication
-    client = AzureOpenAI( 
+client = AzureOpenAI( 
         azure_endpoint = endpoint,
         api_key = subscription_key,
         api_version = "2024-05-01-preview",
     )
 
-    completion = client.chat.completions.create(
-        model=deployment,
-        messages= [
-        {
-            "role": "system",
-            "content": "You are an AI assistant that helps people find information."
-        }
-    ],
-        past_messages=2,
-        max_tokens=800,
-        temperature=0.7,
-        top_p=0.95,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=None,
-        stream=False
-    )
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "..."
 
-def handle_chat_prompt(prompt, deployment, subscription_key, endpoint):
-    # echo the user's prompt to the chat window
-    st.session_state.messages.append({"role":"user", "content":"prompt"})
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
-
-
-
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+        
+    st.session_state.messages.append({"role": "assistant", "content": response})
