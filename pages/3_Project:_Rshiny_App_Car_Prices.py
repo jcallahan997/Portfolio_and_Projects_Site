aa730836e3a0de6c9b91e4d3d35ed1953c6be13b
jcallahan997 <jac421@pitt.edu>
2024-09-24 13:18:50 -0400

    set splash page title

--- pages/3_Project:_Rshiny_App_Car_Prices.py
+++ pages/3_Project:_Rshiny_App_Car_Prices.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 import streamlit.components.v1 as components
 st.header("An Rshiny app presented here through an iframe. The app itself is hosted on Shinyapps, and explores the relationship between car prices and mileage")
 components.iframe("https://fxyqh7-james-callahan.shinyapps.io/car_shinyapp/", height=1950)
