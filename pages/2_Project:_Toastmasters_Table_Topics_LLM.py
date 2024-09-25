a43c19b103e1242d608ddfd721e9cec09b2dbd62
jcallahan997 <jac421@pitt.edu>
2024-09-25 13:13:40 -0400

    add sample code

--- pages/2_Project:_Toastmasters_Table_Topics_LLM.py
+++ pages/2_Project:_Toastmasters_Table_Topics_LLM.py
@@ -20,39 +20,20 @@ st.write("This app is an Azure OpenAI instance of gpt-35-turbo, that is then set
 
 key = os.getenv('API_KEY')
 endpoint = os.getenv('endpoint')
-model_name = os.getenv('deployment_name')
+deployment_name = os.getenv('deployment_name')
 
-
-openai.api_type = "azure"
-openai.api_base = endpoint
-openai.api_key = key
-openai.api_version = "2024-05-01-preview"
-
-client = AzureOpenAI(azure_endpoint=endpoint,api_version="2024-05-01-preview",api_key=key)
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+def create_chat_completion(deployment_name, messages, endpoint, key):
+    client = openai.AzureOpenAI(
+        api_key=key,
+        api_version="2024-05-01-preview",
+        azure_endpoint = endpoint
+    )
+    # Create and return a new chat completion request
+    return client.chat.completions.create(
+        model=deployment_name,
+        messages=[
+            {"role": m["role"], "content": m["content"]}
+            for m in messages
+        ],
+        stream=True
+    )
