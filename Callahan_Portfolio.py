dedf4d6679ba6767eb5c0b52faedf200419f943d
jcallahan997 <jac421@pitt.edu>
2025-11-30 09:10:29 -0500

    change some verbiage of main page

--- Callahan_Portfolio.py
+++ Callahan_Portfolio.py
@@ -5,10 +5,10 @@ st.set_page_config( page_title="Callahan Portfolio", )
 st.title("Welcome to my portfolio!")
 
 st.write("Hi, I'm James. I work as a data scientist and am passionate about\
-          the field. In my free time, I like to boulder, travel, and dance! Working on cars\
+          the field. In my free time, I like to work out, travel, and latin dance! Working on cars\
          and reading are some other things that bring me joy.")
 #add pic of myself here
-st.image("IMG_5550.jpeg", caption="A picture of me on a recent trip I took with a friend to Sri Lanka.")
+st.image("IMG_5550.jpeg", caption="A picture of me on a trip to Sri Lanka.")
 # present resume and a download button for it here
 
 pdf_viewer("Callahan_James_Resume (v3.0) copy.pdf")
@@ -16,8 +16,6 @@ pdf_viewer("Callahan_James_Resume (v3.0) copy.pdf")
 with open('Callahan_James_Resume (v3.0) copy.pdf', 'rb') as f:
    st.download_button('Download resume as .pdf', f, file_name='Callahan_James_Resume.pdf')
 
-# with open('Callahan_James_Resume(v1.0).docx', 'rb') as f:
-#  st.download_button('Download resume as .docx', f, file_name='Callahan_James_Resume.docx')
-
+st.write("Check out my LinkedIn: https://www.linkedin.com/in/jamesacallahan/")
 st.write("Check out my GitHub: https://github.com/jcallahan997")
 st.write("Thank you for visiting my page!")
